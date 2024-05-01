56babf92143338e3b2998244e0f1a235557b6e54
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2024-05-01 11:53:54 +0100

    refactor: removed unused code

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,24 +1,6 @@
 from appconfig.config import Config
 
 
-# def mock_get_users(role):
-#     return [
-#         {
-#             "name": "ricer",
-#             "role": "DST",
-#             "serverParks": [
-#                 "gusty",
-#             ],
-#             "defaultServerPark": "gusty",
-#         },
-#         {
-#             "name": "willij",
-#             "role": role,
-#             "serverParks": ["gusty", "cma"],
-#             "defaultServerPark": "gusty",
-#         },
-#     ]
-
 def get_default_config():
     return Config(
         blaise_api_url="blaise_api_url",
