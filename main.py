73a491b1ccf90aac5920355f57e1583d9f6581ee
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2024-05-02 12:04:27 +0100

    tests: add cov

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def create_ips_donor_cases_processor(request: flask.Request):
 
     guid_service = GUIDService(blaise_service)
     user_service = UserService(blaise_service)
-    donor_service = DonorCaseService(blaise_service)
+    donor_case_service = DonorCaseService(blaise_service)
 
     request_json = request.get_json()
     questionnaire_name = request_json["questionnaire_name"]
@@ -26,7 +26,7 @@ def create_ips_donor_cases_processor(request: flask.Request):
 
     guid = guid_service.get_guid(blaise_server_park, questionnaire_name)
     users_with_role = user_service.get_users_by_role(role)
-    donor_service.create_donor_case_for_users(
+    donor_case_service.create_donor_case_for_users(
         questionnaire_name,
         guid,
         users_with_role
