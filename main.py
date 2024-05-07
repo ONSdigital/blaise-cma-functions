a44c86e65711a88e617ded3a43bbad5457eb009e
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2024-05-07 16:52:31 +0100

    add logs

--- main.py
+++ main.py
@@ -10,27 +10,31 @@ from services.user_service import UserService
 
 
 def create_ips_donor_cases_processor(request: flask.Request):
-    logging.info("Running Cloud Function - create_ips_donor_cases")
+    try:
+        logging.info("Running Cloud Function - create_ips_donor_cases")
 
-    blaise_config = Config.from_env()
-    blaise_service = BlaiseService(config=blaise_config)
+        blaise_config = Config.from_env()
+        blaise_service = BlaiseService(config=blaise_config)
 
-    blaise_server_park = blaise_config.blaise_server_park
+        blaise_server_park = blaise_config.blaise_server_park
 
-    guid_service = GUIDService(blaise_service)
-    user_service = UserService(blaise_service)
-    donor_case_service = DonorCaseService(blaise_service)
+        guid_service = GUIDService(blaise_service)
+        user_service = UserService(blaise_service)
+        donor_case_service = DonorCaseService(blaise_service)
 
-    request_json = request.get_json()
-    questionnaire_name = request_json["questionnaire_name"]
-    logging.info("Questionnaire name: " + questionnaire_name)
-    role = request_json["role"]
-    logging.info("role: " + role)
+        request_json = request.get_json()
+        questionnaire_name = request_json["questionnaire_name"]
+        logging.info("Questionnaire name: " + questionnaire_name)
+        role = request_json["role"]
+        logging.info("role: " + role)
 
-    guid = guid_service.get_guid(blaise_server_park, questionnaire_name)
-    logging.info("GUID: " + guid)
-    users_with_role = user_service.get_users_by_role(blaise_server_park, role)
-    donor_case_service.create_donor_case_for_users(
-        questionnaire_name, guid, users_with_role
-    )
-    return "Done!", 200
+        guid = guid_service.get_guid(blaise_server_park, questionnaire_name)
+        logging.info("GUID: " + guid)
+        users_with_role = user_service.get_users_by_role(blaise_server_park, role)
+        donor_case_service.create_donor_case_for_users(
+            questionnaire_name, guid, users_with_role
+        )
+        return "Done!", 200
+    except Exception as e:
+        logging.error(f"Error creating IPS donor cases: {e}")
+        return "Error creating IPS donor cases", 500
