445507480eb574fb8df5ea22ee0b048cee5be8b6
cal-nic <callum.nicholson@ons.gov.uk>
2024-05-21 15:10:52 +0100

    fix: change logging type to see difference

--- utilities/logging.py
+++ utilities/logging.py
@@ -1,7 +1,7 @@
-from google.cloud.logging_v2.handlers import (StructuredLogHandler,
+from google.cloud.logging_v2.handlers import (CloudLoggingHandler,
                                               setup_logging)
 
 
 def setup_logger():
-    handler = StructuredLogHandler()
+    handler = CloudLoggingHandler()
     setup_logging(handler)
