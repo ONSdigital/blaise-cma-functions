6348b8e081951de328aae3733a002a70ec8f8f69
cal-nic <callum.nicholson@ons.gov.uk>
2024-05-21 15:18:19 +0100

    fix: fix logging with cloudlogginghandlers

--- utilities/logging.py
+++ utilities/logging.py
@@ -1,7 +1,12 @@
+import logging
+import google.cloud.logging
 from google.cloud.logging_v2.handlers import (CloudLoggingHandler,
                                               setup_logging)
 
 
 def setup_logger():
-    handler = CloudLoggingHandler()
+    client = google.cloud.logging.Client()
+    handler = CloudLoggingHandler(client)
     setup_logging(handler)
+    logging.getLogger().setLevel(logging.DEBUG)
+
