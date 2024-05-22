c4ec318dda1527ee99f8093dae2864b0ad41cbde
cal-nic <callum.nicholson@ons.gov.uk>
2024-05-22 11:36:13 +0100

    chore: remove comments

--- utilities/logging.py
+++ utilities/logging.py
@@ -2,8 +2,5 @@ from google.cloud import logging
 
 
 def setup_logger():
-    # Instantiates a client
     client = logging.Client()
-
-    # Connects the logger to the root logging handler
     client.setup_logging()
