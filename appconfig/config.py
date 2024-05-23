4646fbad654518011ba37465da3656ac2cb8bce7
SidraJaved <sidrajaved92@gmail.com>
2024-05-23 11:27:54 +0100

    chore: formatting and reverting few changes in config file

--- appconfig/config.py
+++ appconfig/config.py
@@ -6,16 +6,10 @@ from dataclasses import dataclass
 class Config:
     blaise_api_url: str
     blaise_server_park: str
-    gcloud_project: str
-    region: str
-    cloud_function_sa: str
 
     @classmethod
     def from_env(cls):
         return cls(
             blaise_api_url=os.getenv("BLAISE_API_URL"),
             blaise_server_park=os.getenv("BLAISE_SERVER_PARK"),
-            gcloud_project=os.getenv("GCLOUD_PROJECT"),
-            region=os.getenv("REGION"),
-            cloud_function_sa=os.getenv("CLOUD_FUNCTION_SA"),
         )
--- tests/helpers.py
+++ tests/helpers.py
@@ -2,4 +2,7 @@ from appconfig.config import Config
 
 
 def get_default_config():
-    return Config(blaise_api_url="blaise_api_url", blaise_server_park="gusty", cloud_function_sa="cma-sa@ons-blaise-v2-dev-rr3.iam.gserviceaccount.com", gcloud_project="ons-blaise-v2-dev-rr3",region="europe-west2")
+    return Config(
+        blaise_api_url="blaise_api_url",
+        blaise_server_park="gusty",
+    )
