1bf24ce73947d5e5fd5e1b1016f202f58d574c9d
SidraJaved <sidrajaved92@gmail.com>
2024-05-23 11:11:11 +0100

    dummy values for tests

--- tests/helpers.py
+++ tests/helpers.py
@@ -2,4 +2,4 @@ from appconfig.config import Config
 
 
 def get_default_config():
-    return Config(blaise_api_url="blaise_api_url", blaise_server_park="gusty")
+    return Config(blaise_api_url="blaise_api_url", blaise_server_park="gusty", cloud_function_sa="cma-sa@ons-blaise-v2-dev-rr3.iam.gserviceaccount.com", gcloud_project="ons-blaise-v2-dev-rr3",region="europe-west2")
