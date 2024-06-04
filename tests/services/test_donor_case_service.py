0efa9cdb2ee5fb658356e0aa80d0ef48fc883443
elthorne <elinor.thorne@ons.gov.uk>
2024-06-04 12:27:41 +0100

    refactor: extract OG tests to class
    refs: BLAIS5-4239

--- tests/services/test_donor_case_service.py
+++ tests/services/test_donor_case_service.py
@@ -23,110 +23,108 @@ def donor_case_service(blaise_service) -> DonorCaseService:
     return DonorCaseService(blaise_service=blaise_service)
 
 
-def test_donor_case_does_not_exist_returns_false_if_donor_case_exists(
-    donor_case_service,
-):
-    # Act
-    user = "rich"
-    users_with_existing_donor_cases = ["rich", "sarah"]
+class TestDonorCaseDoesNotExist:
+    def test_donor_case_does_not_exist_returns_false_if_donor_case_exists(
+            self, donor_case_service
+    ):
+        # Act
+        user = "rich"
+        users_with_existing_donor_cases = ["rich", "sarah"]
 
-    # Arrange
-    result = donor_case_service.donor_case_does_not_exist(
-        user, users_with_existing_donor_cases
-    )
-
-    # Assert
-    assert result is False
-
-
-def test_donor_case_does_not_exist_returns_True_if_donor_case_does_not_exist(
-    donor_case_service,
-):
-    # Act
-    user = "james"
-    users_with_existing_donor_cases = ["rich", "sarah"]
-
-    # Arrange
-    result = donor_case_service.donor_case_does_not_exist(
-        user, users_with_existing_donor_cases
-    )
-
-    # Assert
-    assert result is True
-
-
-def test_donor_case_does_not_exist_returns_true_if_an_empty_list_is_provided(
-    donor_case_service,
-):
-    # Act
-    user = "james"
-    users_with_existing_donor_cases = []
-
-    # Arrange
-    result = donor_case_service.donor_case_does_not_exist(
-        user, users_with_existing_donor_cases
-    )
-
-    # Assert
-    assert result is True
-
-
-def test_donor_case_exists_logs_correct_information_when_case_exists(
-    donor_case_service, caplog
-):
-    # Act
-    user = "rich"
-    users_with_existing_donor_cases = ["rich", "sarah"]
-
-    # Arrange
-    with caplog.at_level(logging.INFO):
-        donor_case_service.donor_case_does_not_exist(
+        # Arrange
+        result = donor_case_service.donor_case_does_not_exist(
             user, users_with_existing_donor_cases
         )
 
-    # Assert
-    assert (
-        "root",
-        logging.INFO,
-        "Donor case already exists for user 'rich'",
-    ) in caplog.record_tuples
+        # Assert
+        assert result is False
 
+    def test_donor_case_does_not_exist_returns_true_if_donor_case_does_not_exist(
+        self, donor_case_service
+    ):
+        # Act
+        user = "james"
+        users_with_existing_donor_cases = ["rich", "sarah"]
 
-def test_donor_case_exists_logs_correct_information_when_case_does_not_exist(
-    donor_case_service, caplog
-):
-    # Act
-    user = "james"
-    users_with_existing_donor_cases = ["rich", "sarah"]
-
-    # Arrange
-    with caplog.at_level(logging.INFO):
-        donor_case_service.donor_case_does_not_exist(
+        # Arrange
+        result = donor_case_service.donor_case_does_not_exist(
             user, users_with_existing_donor_cases
         )
 
-    # Assert
-    assert (
-        "root",
-        logging.INFO,
-        "Donor case does not exist for user 'james'",
-    ) in caplog.record_tuples
+        # Assert
+        assert result is True
 
+    def test_donor_case_does_not_exist_returns_true_if_an_empty_list_is_provided(
+        self, donor_case_service
+    ):
+        # Act
+        user = "james"
+        users_with_existing_donor_cases = []
 
-def test_donor_case_exists_logs_an_error_message(donor_case_service, caplog):
-    # Act
-    user = "james"
-    users_with_existing_donor_cases = None
-
-    # Arrange
-    with caplog.at_level(logging.INFO):
-        donor_case_service.donor_case_does_not_exist(
+        # Arrange
+        result = donor_case_service.donor_case_does_not_exist(
             user, users_with_existing_donor_cases
         )
 
-    # Assert
-    assert (
-        "root",
-        40,
-        "Error checking donor case exists for james: argument of type 'NoneType' is not iterable",
-    ) in caplog.record_tuples
+        # Assert
+        assert result is True
+
+    def test_donor_case_exists_logs_correct_information_when_case_exists(
+        self, donor_case_service, caplog
+    ):
+        # Act
+        user = "rich"
+        users_with_existing_donor_cases = ["rich", "sarah"]
+
+        # Arrange
+        with caplog.at_level(logging.INFO):
+            donor_case_service.donor_case_does_not_exist(
+                user, users_with_existing_donor_cases
+            )
+
+        # Assert
+        assert (
+            "root",
+            logging.INFO,
+            "Donor case already exists for user 'rich'",
+        ) in caplog.record_tuples
+
+    def test_donor_case_exists_logs_correct_information_when_case_does_not_exist(
+        self, donor_case_service, caplog
+    ):
+        # Act
+        user = "james"
+        users_with_existing_donor_cases = ["rich", "sarah"]
+
+        # Arrange
+        with caplog.at_level(logging.INFO):
+            donor_case_service.donor_case_does_not_exist(
+                user, users_with_existing_donor_cases
+            )
+
+        # Assert
+        assert (
+            "root",
+            logging.INFO,
+            "Donor case does not exist for user 'james'",
+        ) in caplog.record_tuples
+
+    def test_donor_case_exists_logs_an_error_message(
+        self, donor_case_service, caplog
+    ):
+        # Act
+        user = "james"
+        users_with_existing_donor_cases = None
+
+        # Arrange
+        with caplog.at_level(logging.INFO):
+            donor_case_service.donor_case_does_not_exist(
+                user, users_with_existing_donor_cases
+            )
+
+        # Assert
+        assert (
+            "root",
+            40,
+            "Error checking donor case exists for james: argument of type 'NoneType' is not iterable",
+        ) in caplog.record_tuples
