6b67a7ae3eec6eaf1dd349c0395d98d1bc621436
elthorne <elinor.thorne@ons.gov.uk>
2024-06-05 15:44:07 +0100

    test: assert exception raised
    refs: BLAIS5-4239

--- tests/services/test_user_service.py
+++ tests/services/test_user_service.py
@@ -1,3 +1,4 @@
+import logging
 from unittest import mock
 
 import pytest
@@ -6,6 +7,7 @@ from appconfig.config import Config
 from services.blaise_service import BlaiseService
 from services.user_service import UserService
 from tests.helpers import get_default_config
+from utilities.custom_exceptions import BlaiseError, UsersError
 
 
 @pytest.fixture()
@@ -81,3 +83,30 @@ def test_get_users_by_role_returns_empty_list_when_no_users_are_found_with_a_giv
     # Assert
     assert len(result) == 0
     assert result == []
+
+
+@mock.patch.object(BlaiseService, "get_users")
+def test_get_users_by_role_logs_and_raises_a_blaise_error_exception_when_get_users_fails_with_blaise_error(
+    get_users, user_service, caplog
+):
+    # Arrange
+    get_users.side_effect = BlaiseError("All the rum has gone and Jack Sparrow doesn't understand why?")
+    role = "IPS Field Interviewer"
+    blaise_server_park = "gusty"
+
+    # Act
+    with pytest.raises(BlaiseError) as err:
+        user_service.get_users_by_role(blaise_server_park, role)
+
+    # Assert
+    error_message = (
+        "BlaiseError caught in UserService.get_users_by_role(). "
+        f"Error getting users by role for server park gusty: "
+        f"All the rum has gone and Jack Sparrow doesn't understand why?"
+    )
+    assert err.value.args[0] == error_message
+    assert (
+               "root",
+               logging.ERROR,
+               error_message,
+           ) in caplog.record_tuples
