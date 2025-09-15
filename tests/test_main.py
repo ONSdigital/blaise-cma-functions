6e60862d9d58a84728c5b06196c0c63885ffe4e4
sharaz15 <shahraz15@gmail.com>
2025-09-15 11:15:32 +0100

    updated assertion to match id

--- tests/test_main.py
+++ tests/test_main.py
@@ -888,7 +888,7 @@ class TestMainReissueNewDonorCasesHandleRequestStep:
         called_model = mock_create_donor_case_for_user.call_args[0][0]
         
         assert called_model.user == "rich"
-        assert called_model.questionnaire_name == "LMS2309_GO1"
+        assert called_model.questionnaire_name == "IPS2402a"
         assert called_model.guid == "25615bf2-f331-47ba-9d05-6659a513a1f2"
 
     @mock.patch.object(blaise_restapi.Client, "get_questionnaire_for_server_park")
